b867250cc538b32e6a5c2d65372b79bcae4725cb
Momtchil Momtchev <momtchil@momtchev.com>
2020-10-01 12:03:59 +0200

    resiliency when reloading with invalid config

--- src/config.js
+++ src/config.js
@@ -1,10 +1,16 @@
 import * as fs from 'fs';
+import * as geodb from './geodb.js';
+import * as altdb from './altdb.js';
 
 export let config;
 let confName;
 
 export function loadConfig(altConf) {
     !confName && (confName = altConf || 'config.json');
-    config = JSON.parse(fs.readFileSync(confName));
+	const newConf = JSON.parse(fs.readFileSync(confName));
+	geodb.loadConfig(newConf);
+	altdb.loadConfig(newConf);
+
+	config = newConf;
     return config;
 }
